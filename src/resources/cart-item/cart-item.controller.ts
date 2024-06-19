6201b1b74cef1cc7a7bdea755d616111ec381729
youssef-rafie212 <youssefelbosaty3@gmail.com>
2024-06-19 09:02:51 +0300

    p1

--- src/resources/cart-item/cart-item.controller.ts
+++ src/resources/cart-item/cart-item.controller.ts
@@ -2,7 +2,7 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { CartItemService } from './cart-item.service';
 import { CreateCartItemDto } from './dto/createCartItem.dto';
 
-@Controller('/api/cart-item')
+@Controller('/api/cart/add') // Adds a cart item (product) to the given cart
 export class CartItemController {
   constructor(private readonly cartItemService: CartItemService) {}
 
