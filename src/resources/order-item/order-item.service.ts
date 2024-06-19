87141d68b68991009fc24183036672e8652a7a10
youssef-rafie212 <youssefelbosaty3@gmail.com>
2024-06-19 22:55:08 +0300

    final implementation

--- src/resources/order-item/order-item.service.ts
+++ src/resources/order-item/order-item.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateOrderItemDto } from './dto/createOrderItem.dto';
 import { PrismaService } from 'prisma/prisma.service';
 
@@ -6,7 +6,25 @@ import { PrismaService } from 'prisma/prisma.service';
 export class OrderItemService {
   constructor(private prisma: PrismaService) {}
 
-  createOrderItem(createOrderItemDto: CreateOrderItemDto) {
+  async createOrderItem(createOrderItemDto: CreateOrderItemDto) {
+    // Update the total price of the related order
+    const order = await this.prisma.order.findUnique({
+      where: { id: createOrderItemDto.orderId },
+    });
+
+    if (!order) {
+      throw new BadRequestException();
+    }
+
+    const currentTotalPrice = order.totalPrice;
+
+    const total = createOrderItemDto.unitPrice * createOrderItemDto.quantity;
+
+    await this.prisma.order.update({
+      where: { id: createOrderItemDto.orderId },
+      data: { totalPrice: currentTotalPrice + total },
+    });
+
     return this.prisma.orderItem.create({
       data: createOrderItemDto,
     });
