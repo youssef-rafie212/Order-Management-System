004e0241e685b9f5eff0564b1a88704c05588d51
youssef-rafie212 <youssefelbosaty3@gmail.com>
2024-06-23 09:40:14 +0300

    C12

--- src/resources/order/order.service.ts
+++ src/resources/order/order.service.ts
@@ -69,6 +69,9 @@ export class OrderService {
     return await this.prisma.order.update({
       where: { id: orderId },
       data: { status: newStatus },
+      include: {
+        orderItems: true,
+      },
     });
   }
 
@@ -87,6 +90,7 @@ export class OrderService {
     return await this.prisma.order.update({
       where: { id: orderId },
       data: { totalPrice: oldTotalPrice / 2 },
+      include: { orderItems: true },
     });
   }
 }
