687c04d954de2c6f50d3fa3e6c17fad882c3acef
youssef-rafie212 <youssefelbosaty3@gmail.com>
2024-06-23 09:24:41 +0300

    Final

--- src/resources/order-item/order-item.module.ts
+++ src/resources/order-item/order-item.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
 import { OrderItemService } from './order-item.service';
-import { OrderItemController } from './order-item.controller';
 
 @Module({
-  controllers: [OrderItemController],
+  controllers: [], // This resource has no controllers as the service is only used by the order service
   providers: [OrderItemService],
   exports: [OrderItemService],
 })
--- src/resources/order-item/order-item.service.ts
+++ src/resources/order-item/order-item.service.ts
@@ -6,6 +6,7 @@ import { PrismaService } from 'prisma/prisma.service';
 export class OrderItemService {
   constructor(private prisma: PrismaService) {}
 
+  // This method is intended to be used by the order service (no controllers)
   async createOrderItem(createOrderItemDto: CreateOrderItemDto) {
     return await this.prisma.orderItem.create({
       data: createOrderItemDto,
